4cc6d3637d43c1148777837147124f85e5af4513
Zarreen-Shaikh-12 <zarreen.shaikh.cmaug26@gmail.com>
2026-09-20 00:24:34 +0530

    Add files via upload

---
+++ Assignments/OOPJ/ForLoopExercise/OddNumbers.java
@@ -0,0 +1,23 @@
+
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 1000; i++) {
+			if(i % 2 != 0) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+}
+
+
+//public class OddNumbers
+//{
+//    public static void main(String[] args)
+//    {
+//        for(int i = 1; i <= 1000; i = i + 2)
+//        {
+//            System.out.println(i);
+//        }
+//    }
+//}
---
+++ Assignments/OOPJ/ForLoopExercise/SeventhNumber.java
@@ -0,0 +1,8 @@
+
+public class SeventhNumber {
+	public static void main(String[] args) {
+		for (int i = 7; i <= 200; i = i + 7) {
+			System.out.print(i + " ");
+	    }
+	}
+}
