dc1cad4201c5f9f7c0b3cbf6b359bc7483baec0e
Zarreen-Shaikh-12 <zarreen.shaikh.cmaug26@gmail.com>
2026-09-21 01:23:15 +0530

    Add files via upload

---
+++ Assignments/OOPJ/StringsExercise/Exercise5.java
@@ -0,0 +1,14 @@
+package com.zarreen;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+
+        String str = "The quick brown fox jumps over the lazy dog.";
+
+        System.out.println("Original string: " + str);
+
+        String newStr = str.replaceAll("fox", "cat");
+
+        System.out.println("New String: " + newStr);
+    }
+}
---
+++ Assignments/OOPJ/StringsExercise/Exercise6.java
@@ -0,0 +1,14 @@
+package com.zarreen;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+
+        String str = "The Quick BroWn FoX!";
+
+        System.out.println("Original String: " + str);
+
+        String upper = str.toUpperCase();
+
+        System.out.println("String in uppercase: " + upper);
+    }
+}
